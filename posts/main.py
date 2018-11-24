7b0e1a4f2caeed07578d53b6853815845804c5e2
imildositoe <imildositoe@gmail.com>
2018-11-24 23:19:28 +0000

    UI for login finished

--- posts/main.py
+++ posts/main.py
@@ -1,34 +1,3 @@
-# import requests
-# import json
-#
-# # if __name__ == '__main__':
-# #     url = 'http://127.0.0.1:8080/dawapi'
-# #     response = requests.get(url)
-# #
-# #     print(response)
-#
-# if __name__ == '__main__':
-#     url = 'http://127.0.0.1:8080/dawapi/usuario/login'
-#     payload = {
-#         'usuario': 'grupo',
-#         'senha': 'primeiro'
-#     }
-#     headers = {
-#         'Content-type': 'application/json',
-#         'Accept': 'application/json'}
-#     response = requests.post(url, data=json.dumps(payload), headers=headers)
-#     # print(response.text)
-#
-#     url2 = 'http://127.0.0.1:8080/dawapi/usuario/1'
-#     headers2 = {
-#         'Content-Type': 'application/json',
-#         'Accept': 'application/json',
-#         'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJub21lIjoiR3J1cG8gUHJpbWVpcm8iLCJpZCI6IjEiLCJleHAiOjE1NDMxNDk5Mzl9.VFLM57XG7HeUx2YW9n63rRVqI6DY9lhplyY6IxsUhRjrtepjcnhyG3zCLHUlTiL3POA-UbvHKKNLwayQxZMrmw'
-#     }
-#     response2 = requests.get(url, headers=headers2)
-#
-#     print(response2.content)
-
 import requests
 import json
 
